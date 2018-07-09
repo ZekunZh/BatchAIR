822c01695f8f8ebdc0004a36db4cab1acc7434d0
angusrtaylor <angusrtaylor@gmail.com>
2018-07-09 03:27:18 +0000

    removed libPath append

--- R/mnist_cnn.R
+++ R/mnist_cnn.R
@@ -1,8 +1,4 @@
 
-# add mlserver libraries to libPaths (for rstudio on dsvm)
-#.libPaths(c(.libPaths(), "/data/mlserver/9.2.1/libraries/RServer/"))
-#.libPaths()
-
 library(keras)
 library(argparse)
 library(jsonlite)
