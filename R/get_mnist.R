55d1cf15af5ae6b271804b93aecacfd8e147454c
angusrtaylor <angusrtaylor@gmail.com>
2018-07-09 03:26:16 +0000

    update file path

--- R/get_mnist.R
+++ R/get_mnist.R
@@ -1,10 +1,9 @@
 
 # add mlserver libraries to libPaths (for rstudio on dsvm)
 .libPaths(c(.libPaths(), "/data/mlserver/9.2.1/libraries/RServer/"))
-.libPaths()
 
 library(keras)
 
 mnist <- dataset_mnist()
 
-saveRDS(mnist, file = './mnist.rds')
+saveRDS(mnist, file = file.path('mnist', 'mnist.rds'))
